798e1ed3bc0c592f9422233dfa7bf3bf12f6a64b
Daksh Agrawal <daksh.agrawal@outlook.com>
2025-09-06 20:31:43 -0700

    Committing-new-folders

---
+++ Math problem.java/MathProblems.java
@@ -0,0 +1,40 @@
+//Daksh Agrawal
+//9-4-2025 Per7
+//MathProblems.java
+/* This program solves the math problems assigned in class.
+ * 
+ * 
+ * 
+ * Working on:
+ * Order of operations, math functions [+, -, *, /, %], 
+ * creating differnet variable types like: float, char, , etc.
+ */
+
+public class MathProblems
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\n\n\n");
+		
+		//initializing variables
+		float num1 = 0;
+		float num2 = 0;
+		char num3 = ' ';
+		
+		//printing results
+		System.out.printf("%s","1) String of numbers="); //question 1
+		
+		System.out.println("1) String of numbers="+Format()); //question 1
+		
+		System.out.printf("%s","2) String of numbers="); //question 2
+		
+		System.out.printf("%s","3) String of numbers="); //question 3
+		
+		System.out.print("4) String of numbers="+Format()); //question 4
+		
+		System.out.printf("%s","5) String of numbers="); //question 5
+		
+		System.out.println("1) String of numbers="+Format()); //question 1
+		
+	}
+}
