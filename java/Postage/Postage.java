dcbee1aefd1668333b9f1816a9c66c7fa0807fc6
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2025-10-30 15:50:37 -0700

    .

--- java/Postage/Postage.java
+++ java/Postage/Postage.java
@@ -8,9 +8,9 @@
  * 
  * Working on: If-else statements
  * 
- * Testing: ONly integers btween 1&4 (inclusive) should give a line,
+ * Testing: Only integers between 1&4 (inclusive) should give a line,
  * 	everything else gives an error message 
- * 	try 1-4, and -2, 14
+ * 	try 1-4 (wont give error), and -2, 14 (will give error)
  */
 
 import java.util.Scanner;
@@ -21,15 +21,15 @@ public class Postage
 	private int weight;
 	private double cost;
 	
-	public Postage
+	public Postage()
 	{
 		//blank 
 	}
 	
-	public static void Main(String[] args)
+	public static void main(String[] args)
 	{
 		Postage post = new Postage();
-		Postage.sendThis();
+		post.sendThis();
 	}
 	
 	public void sendThis()
@@ -41,43 +41,106 @@ public class Postage
 	
 	public void printInfo()
 	{
-		if (category == 1)
+		if (category == 1 || category == 2 || category == 3)
 		{
-			//print values
-		}
-		else if (category == 2)
-		{
-			//print values
-		}
-		else if (category == 3)
-		{
-			//print values
+			System.out.printf("%n%n%-33s","Your postage class:");
+			if (category == 1)
+			{
+				System.out.print("First class, domestic\n");
+				if (weight == 1)
+					System.out.printf("%-33s%s%n","Weight:",(weight+" ounce"));
+				else
+					System.out.printf("%-33s%s%n","Weight:",(weight+" ounces"));
+			}
+			else if (category == 2)
+			{
+				System.out.print("Postcards, domestic\n");
+				//print values
+			}
+			else if (category == 3)
+			{
+				System.out.print("Media Mail\n");
+				if (weight == 1)
+					System.out.printf("%-33s%s%n","Weight:",(weight+" pound"));
+				else
+					System.out.printf("%-33s%33s%n","Weight:",(weight+" pounds"));
+			}
+			System.out.printf("%-33s$%.2f%n","Charge:",cost);
 		}
 		
-		System.out.print("Have a nice day!");
+		System.out.print("\nHave a nice day!");
 		System.out.print("\n\n\n");
 	}
 	
 	public void calculateCost()
 	{
+		//costs
+		final double FIRSTOUNCECOST = 0.78;
+		final double ADDITIONALOUNCECOST = 0.29;
+		final double ANYPOSTCARDCOST = 0.61;
+		final double FIRSTPOUNDCOST = 4.47;
+		final double ADDITIONALPOUNDCOST = 0.72;
+		
 		if (category == 1)
 		{
-			cost = 0.79 + (weight-1)*0.29;
+			cost = FIRSTOUNCECOST + (weight-1)*ADDITIONALOUNCECOST;
 		}
 		else if (category == 2)
 		{
-			cost = 0.61
+			cost = ANYPOSTCARDCOST;
 		}
 		else if (category == 3)
 		{
-			cost = 4.41 + (weight-1)*0.72;
+			cost = FIRSTPOUNDCOST + (weight-1)*ADDITIONALPOUNDCOST;
 		}
 	}
 	
 	public void getData()
 	{
+		String categoryTest = new String();
+		String weightTest = new String();
+		
 		Scanner input = new Scanner(System.in);
 		System.out.print("\n\n\n");
 		
-		System.out.print("Eneter category");
+		System.out.println("Hello, and welcome to the US Post Office.");
+		System.out.println("\n\t(1) First class, domestic");
+		System.out.println("\t(2) Postcards, domestic");
+		System.out.println("\t(3) Media Mail");
+		
+		System.out.printf("%n%-65s %-5s",("Using the menu above, please enter the "
+						+ "category of your postage"),"->");
+		categoryTest = input.nextLine();	
+		
+		if (categoryTest.equals("1"))
+		{
+			category = 1;
+			System.out.printf("%-65s %-5s",("Enter the weight, in ounces,"
+						+ " of your letter (an integer)"),"->");
+			weightTest = input.nextLine();
+		}
+		else if (categoryTest.equals("2")) 
+		{
+			category = 2;
+			//no question for weight
+		}
+		else if (categoryTest.equals("3")) 
+		{
+			category = 3;
+			System.out.printf("%-65s %-5s",("Enter the weight, in pounds,"
+						+ " of your package (an integer)"),"->");
+			weightTest = input.nextLine();
+		}
+		else
+		{
+			System.out.println("\nPlease enter a valid postage class!\n");
+		}
+		//!fix the weight thing here for decimal values
+		if ((category == 1 || category == 3) && !weightTest.equals()
+		{
+			System.out.println("The weight you entered is not a valid input. "
+								+ "Rerun the program and input a positive"
+								+ " integer");
+		}
 	}
+}
