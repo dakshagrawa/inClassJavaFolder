e5bbedb3ffc8d6b0c8ce076a1fa566e08d5cc4a8
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2025-10-30 15:50:57 -0700

    .

--- java/Postage/Postage.java
+++ java/Postage/Postage.java
@@ -135,7 +135,7 @@ public class Postage
 		{
 			System.out.println("\nPlease enter a valid postage class!\n");
 		}
-		//!fix the weight thing here for decimal values
+		//! fix the weight thing here for decimal values
 		if ((category == 1 || category == 3) && !weightTest.equals()
 		{
 			System.out.println("The weight you entered is not a valid input. "
