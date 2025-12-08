779fdbc5af5b3c49cb4568679c9f5d8440162d60
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2025-12-08 15:47:18 -0800

    !

---
+++ java/Punctuation/Punctuation.java
@@ -0,0 +1,24 @@
+// Daksh Agrawal
+/* Period 7
+ * 12/08/2025
+ * 
+ * Punctuation.java
+ * 
+ * >Description: 
+ * 
+ * >Learning: 
+ * 
+ * >Pseudocode:
+ * 
+ * >Testing: 
+ * 
+ */
+public class Punctuation
+{
+	// Default constructor (not used here, but included for completeness)
+	public Punctuation()
+	{
+		
+	}
+	
+}
