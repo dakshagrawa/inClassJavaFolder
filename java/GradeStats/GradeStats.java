19d3e1d6c9f3294aee9a78612ce5b770013a89e5
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2026-01-15 15:46:39 -0800

    #

---
+++ java/GradeStats/GradeStats.java
@@ -0,0 +1,32 @@
+// Daksh Agrawal
+/* Period 7
+* 01/15/2026
+* 
+* GradeStats.java
+* 
+* >Objective:
+* 
+* >Description:
+*
+* >Learning:
+*
+* >Pseudocode:
+* 	Import Scanner
+* 	Class Header
+*
+* >Testing:
+*
+*   
+*/
+public class GradeStats
+{
+	public GradeStats()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
