94e5bc6419143bef4154f87f451fa50e7be84e90
Daksh Agrawal <daksh.agrawal@outlook.com>
2026-01-19 17:31:11 -0800

    Add GradeStats class for calculating and displaying grade statistics

--- java/GradeStats/GradeStats.java
+++ java/GradeStats/GradeStats.java
@@ -18,15 +18,162 @@
 *
 *   
 */
+
+import java.util.Scanner;
+
 public class GradeStats
 {
+	private int[] grades;
+	private int numOfScores;
+	private int minimum;
+	private int maximum;
+	private double average;
+	private double median;
+
+
 	public GradeStats()
 	{
-		
+		grades = new int[]{};
 	}
 	
 	public static void main(String[] args)
 	{
-		
+		GradeStats gs = new GradeStats();
+		gs.calculateIt();
+	}
+	
+	public void calculateIt()
+	{
+		System.out.print("\n\n\n");
+		System.out.println("=======================================================================");
+		System.out.println("     Welcome to the Grade Statistics Calculator! (GradeStats.java)     ");
+		System.out.println("=======================================================================");
+		System.out.println("This program is designed to help you analyze class performance. Simply\n"
+						 + "enter your scores one by one. "
+						 + "Once you're done, it will automatically\ncalculate:");
+		System.out.println("  - The Highest Scored Grade (Maximum)");
+		System.out.println("  - The Lowest Scored Grade (Minimum)");
+		System.out.println("  - The Class Average (Mean)");
+		System.out.println("  - The Median Score of the Class");
+		System.out.println("-----------------------------------------------------------------------\n");
+
+		getGrades();
+
+		printScores();
+
+		below75Percent(); //? is it okay to print in this method?
+
+		numOfScores = grades.length;
+		minimum = findMin(grades);
+		maximum = findMax(grades);
+		average = findAvg(grades);
+		median = findMedian(grades);
+
+		printCalculated();
+
+		System.out.print("\n\n\n"); 
+	}
+
+	public void getGrades()
+	{
+		String input;
+		do
+		{
+			Scanner in = new Scanner(System.in);
+			//String input;
+			int[] temp = new int[grades.length+1];
+			
+			System.out.print("Type in the score. Type \"Quit\" to end the program --> ");
+			input = in.next();
+			if (!input.equalsIgnoreCase("quit"))
+			{
+				if(!(grades.length==0))
+					for(int i = 0; i < grades.length; i++)
+					{
+						temp[i] = grades[i];
+					}
+				temp[temp.length-1] = Integer.parseInt(input);
+				grades = temp;
+			}
+
+		}while(!input.equalsIgnoreCase("quit"));
+	}
+
+	public void printScores()
+	{
+		System.out.println("\nHere is the data you entered:");
+
+		for(int i = 0; i < grades.length; i++)
+			System.out.println("Student "+(i+1)+"'s score: "+grades[i]);
+	}
+
+	public void below75Percent()
+	{
+		int totalBelow75 = 0;
+		String studentBelow75 = new String();
+
+		for(int i = 0; i < grades.length; i++)
+		{
+			if (grades[i]<75)
+			{
+				totalBelow75++;
+				studentBelow75 += ("student "+(i+1)+", ");
+			}
+		}
+
+		System.out.println("\nThere were " + totalBelow75
+						 + " students who scored below 75%: "
+						 + studentBelow75.substring(0,studentBelow75.length()-2)
+						 + ".");
+	}
+
+	public int findMin(int[] givenGrades)
+	{
+		int min = givenGrades[0];
+		for (int i = 1; i < givenGrades.length ; i++)
+			min = Math.min(givenGrades[i],min);
+		return min;
+	}
+
+    public int findMax(int[] givenGrades) 
+	{
+		int max = givenGrades[0];
+		for (int i = 1; i < givenGrades.length ; i++)
+			if (max < givenGrades[i])
+				max = givenGrades[i];
+		return max;
+	}
+
+    public double findAvg(int[] givenGrades)
+	{
+		double sum = 0.0;
+		for (int i = 0; i < givenGrades.length ; i++)
+			sum += givenGrades[i];
+		return (sum/givenGrades.length);
+    }
+
+	public double findMedian(int[] givenGrades) 
+	{
+		int med;
+		int[] temp1 = new int[]{};
+		for (int i = 0; i < givenGrades.length; i++)
+		{
+			for (int j = 0; )
+			if (grades[i+1] < grades[i])
+			{
+				
+			}
+			int temp2;
+			
+		}
+    }
+
+	public void printCalculated()
+	{
+		System.out.printf("%n%-20s%d","Number of scores:", numOfScores);
+		System.out.printf("%n%-20s%d","Minimum:", minimum);
+		System.out.printf("%n%-20s%d","Maximum:", maximum);
+		System.out.printf("%n%-20s%.1f","Average:", average);
+		System.out.printf("%n%-20s%.1f","Median:", median);
 	}
 }
