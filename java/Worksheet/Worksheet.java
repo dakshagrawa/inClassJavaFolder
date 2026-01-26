2a9933acb2b648d2885f892b297dc1b46401cacd
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2026-01-26 15:20:33 -0800

    #

---
+++ java/Worksheet/Worksheet.java
@@ -0,0 +1,47 @@
+// Daksh Agrawal
+/* Period 7
+* 01/22/2026
+* 
+* Worksheet.java
+* 
+* >Objective:
+* 
+* >Description:
+*
+* >Learning:
+*
+* >Pseudocode:
+* 		
+*
+* >Testing:
+*
+*   
+*/
+
+import java.util.Scanner;
+
+public class Worksheet
+{
+	private int numOfQuestions;
+	private int j
+	
+	public Worksheet()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		getInputs();
+	}
+	
+	public void getInputs()
+	{
+		
+	}
+	
+	public int randomNumber(int n)
+	{
+		return Math.random();
+	}
+}
