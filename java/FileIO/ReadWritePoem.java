ac7f648c66a698028284e815bb0a16b9573372fa
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2026-01-26 15:50:36 -0800

    #

---
+++ java/FileIO/ReadWritePoem.java
@@ -0,0 +1,90 @@
+// Daksh Agrawal
+/* 01/26/2026 Per 7
+* ReadWritePoem.java
+* Reads a poem in poem.txt, then write the first word of each line to a file 
+* called output.txt
+* First FileIO program
+* Working on: Reading and writing to files compared to the terminal.   
+*/
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class ReadWritePoem
+{
+	private Scanner input; // Scanner obj to read from the file
+	private PrintWriter output; // PrintWriter obj to write to file
+	private String inFileName; // text file to read from
+	private String outFileName; // text file to write to
+	
+	public ReadWritePoem()
+	{
+		input = null;
+		output = null;
+		inFileName = new String("poem.txt");
+		outFileName = new String("output.txt");
+	}
+	
+	public static void main(String[] args)
+	{
+		ReadWritePoem rwp = new ReadWritePoem();
+		rwp.readIt();
+	}
+	
+	public void readIt()
+	{
+		openIt();
+		makeIt();
+		getWords();
+	}
+	
+	/* This method called all of the other ones. There are no local variables */
+	public void openIt()
+	{
+		File inFile = new File(inFileName);
+		try
+		{
+			input = new Scanner(inFile);
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.printf("\n\n\nERROR: Cannot find/open file %s.\n\n\n",
+				inFileName);
+			System.exit(1);
+		}
+	}
+	
+	/* makeIt uses a try-catch block to create a file to write to. */
+	public void makeIt()
+	{
+		File outFile = new File(outFileName);
+		try
+		{
+			output = new PrintWriter(outFile);
+		}
+		catch (IOException e)
+		{
+			System.err.println("\n\n\nERROR: Cannot create " + outFileName +
+				" file.\n\n\n");
+			System.exit(2);
+		}
+	}
+	
+	/* getWords reade the input file, saves the 1st word of each lin, then prints
+	 * that word to the output file. No parameters or return value. */
+	 public void getWords()
+	 {
+		 System.out.println("\n\n\n");
+		 String word = "";
+		 while (input.hasNext())
+		 {
+			 word = input.next();
+			 input.nextLine();
+			 output.println(word);
+		 }
+		 output.close();
+	 }
+}
