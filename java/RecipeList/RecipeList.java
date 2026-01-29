c325fcc0c3e9175dcebb0b7f468f41b0625106ea
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2026-01-29 15:48:57 -0800

    #

---
+++ java/RecipeList/RecipeList.java
@@ -0,0 +1,38 @@
+// Daksh Agrawal
+/* Period 7
+* 01/06/2026
+* 
+* RecipeList.java
+* 
+* >Objective:
+* 
+* >Description:
+*
+* >Learning:
+*
+* >Pseudocode:
+* 		
+*
+* >Testing:
+*
+*   
+*/
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class RecipeList
+{
+	public RecipeList()
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
