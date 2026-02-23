3890e4ab4ca86c05777dffcbb4b8fb6e2b6d35eb
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2026-02-23 15:51:39 -0800

    #

--- java/Masterpiece/Masterpiece.java
+++ java/Masterpiece/Masterpiece.java
@@ -31,7 +31,7 @@ public class Masterpiece
 		JFrame f = new JFrame("Daksh's Masterpiece");
 		
 		f.setSize(1000,500);
-		f.setResizable(true);
+		f.setResizable(false);
 		f.setLocation(0,0);
 		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
 		Panel panel1 = new Panel();
@@ -68,13 +68,40 @@ class Panel extends JPanel
 		g.setColor(Color.BLACK);
 		g.drawOval(400,200,200,200);
 		
+		//tree 1
 		g.setColor(Color.GREEN);
 		g.fillOval(100,300,50,50);
+		g.setColor(Color.BLACK);
+		g.drawOval(100,300,50,50);
 		
 		g.setColor(Color.darkGray);
 		g.fillRect(122,325,6,100);
+		g.setColor(Color.BLACK);
+		g.drawRect(122,325,6,100);
 		
+		//tree 2
+		g.setColor(Color.GREEN);
+		g.fillOval(900,300,60,60);
+		g.setColor(Color.BLACK);
+		g.drawOval(900,300,60,60);
 		
+		g.setColor(Color.darkGray);
+		g.fillRect(927,325,6,100);
+		g.setColor(Color.BLACK);
+		g.drawRect(927,325,6,100);
+		
+		//tree 3
+		g.setColor(Color.GREEN);
+		g.fillOval(700,325,50,50);
+		g.setColor(Color.BLACK);
+		g.drawOval(700,325,50,50);
+		
+		g.setColor(Color.darkGray);
+		g.fillRect(722,350,6,100);
+		g.setColor(Color.BLACK);
+		g.drawRect(722,350,6,100);
+		
+		//mountain
 		g.setColor(Color.darkGray);
 		int[] mount_x = new int[]{225,300,325,400,500,525,600};
 		int[] mount_y = new int[]{400,300,325,200,325,300,400};
@@ -83,6 +110,30 @@ class Panel extends JPanel
 		//Outline
 		g.setColor(Color.BLACK);
 		g.drawPolygon(mount_x,mount_y,7);
+		
+		//stars
+		g.setColor(Color.YELLOW);
+		g.setFont(new Font("Serif",Font.PLAIN, 40));
+		g.drawString("*",100,100);
+		g.drawString("*",150,225);
+		g.drawString("*",200,150);
+		g.drawString("*",550,50);
+		g.drawString("*",650,175);
+		g.drawString("*",800,250);
+		g.drawString("*",825,125);
+		g.drawString("*",900,200);
+		
+		//cloud
+		g.setColor(Color.WHITE);
+		g.fillOval(400,100,100,50);
+		g.fillOval(400,125,100,50);
+		g.fillOval(385,120,30,35);
+		g.fillOval(485,120,30,35);
+		g.setColor(Color.BLACK);
+		g.drawArc(400,100,100,50,170,-165);
+		g.drawArc(400,125,100,50,-170,160);
+		g.drawArc(385,120,30,35,85,195);
+		g.drawArc(485,120,30,35,-100,200);
 		
 	}
 }
