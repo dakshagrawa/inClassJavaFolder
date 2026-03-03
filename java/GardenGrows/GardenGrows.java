fa46cd13ea2ce89a315dd9682fd78c85acfc0781
Daksh <77264604+dakshagrawa@users.noreply.github.com>
2026-03-03 15:50:19 -0800

    #

--- java/GardenGrows/GardenGrows.java
+++ java/GardenGrows/GardenGrows.java
@@ -9,7 +9,20 @@
 * >Learning:
 *
 * >Pseudocode:
-* 			
+* 	import everything
+* 
+* 	GardenGrows class header
+* 		1. main calls constructor and another method
+* 		2. the next method:
+* 			- makes a new JFrame with other required stuff (size, location, etc.)
+* 			- calls Panel class (Garden.java)
+* 			- setVisible
+* 
+* 	Garden.java class header extends JPanel and implements MouseListner and MouseMotionListner
+* 		1. make FVs (include booleans for the sequence of the growing garden)
+* 		2. make PaintComponent and call superclass also call all other methods
+* 		3. draw a pink filled rectangle (1000x50) @(50,50) in a method
+* 		4. in mouseClicked() includes calling another method ()
 *
 * >Testing:
 *
