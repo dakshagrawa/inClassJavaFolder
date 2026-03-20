936a77dffbdecb5ce5b3ed298284f19588abceb1
Daksh Agrawal <daksh.agrawal@outlook.com>
2026-03-19 17:40:17 -0700

    1

--- java/SneezePanels/SneezePanels.java
+++ java/SneezePanels/SneezePanels.java
@@ -14,19 +14,22 @@ and variables are reset.
 /// Try clicking anywhere other that the button.  This should will not change anything.  
 /// Typing anything other than "Bless you" will not reset the panels.
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridLayout;
+import java.awt.event.ActionListener;	
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
+
+import javax.swing.JFrame;	
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import javax.swing.JTextField;
+
+import java.awt.Color;		
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Dimension;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 public class SneezePanels
 {	
